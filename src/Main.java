2523fa10c1583f39b17c155c57055214b7d82307
Scott Pantall <aspantall@gmail.com>
2017-05-16 16:09:57 -0600

    init commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author Scott Pantall
+ */
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        Path file = Paths.get(args[0]);
+        BufferedReader buffer = Files.newBufferedReader(file);
+        String line;
+        
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            System.out.println(line);
+            //rightMostChar(line);
+            
+            
+        }
+    }
+    
+    public static void rightMostChar(String line) {
+        String[] lists = line.split(";"); 
+    }
+}
